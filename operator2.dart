31cbe4dd0fdc6d5395150e6c04efbd3bf8d684a7
Ankush Singh Gandhi <bhanusinghank@gmail.com>
2020-08-28 02:59:15 +0530

    added operator2.dart

---
+++ operator2.dart
@@ -0,0 +1,12 @@
+main(){
+  double type1 = 5.0;
+  int type2 = 87;
+  String type3 = "ankush";
+  bool type4 = true;
+
+  print(type1 is int);
+  print(type2 is int);
+  print(type3 is String);
+  print(type4 is double);
+  print(type4 is! double);
+}
