768b217e339826cc27257b5157cc95290f9848c0
Ankush Singh Gandhi <bhanusinghank@gmail.com>
2020-08-28 02:53:17 +0530

    added operand.dart

---
+++ operator.dart
@@ -0,0 +1,11 @@
+main(){
+  var operand1 = 10;
+  var operand2 = 7;
+
+  print(operand1 + operand2);
+  print(operand1 - operand2);
+  print(operand1 * operand2);
+  print(operand1 / operand2);
+  print(operand1 ~/ operand2);
+  print(operand1 % operand2);
+}
