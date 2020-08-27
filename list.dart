0b16eecedb75118da02f7858f60a464b9cad818c
Ankush Singh Gandhi <bhanusinghank@gmail.com>
2020-08-28 03:06:23 +0530

    added some veggies

---
+++ list.dart
@@ -0,0 +1,8 @@
+main(){
+  var listofvegies = ['potato','carrot'];
+  listofvegies.addall(['cucumber','tomato']);
+  print(listofvegies);
+  listofvegies.removeAt(1);
+  print(listofvegies);
+  listofvegies.clear()
+}
