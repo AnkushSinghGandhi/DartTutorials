913d4d443d1c1a73dcdce06b4ec12b2035fa8b4e
Ankush Singh Gandhi <bhanusinghank@gmail.com>
2020-08-28 02:34:17 +0530

    added strings.dart

---
+++ string.dart
@@ -0,0 +1,13 @@
+main(){
+  // Single Quotes
+  print('Using single quotes');
+
+  // Double Quotes
+  print('Using double quotes');
+
+  // Single Quotes with escape character
+  print('It\'s possible with escape character');
+
+  // Double Quotes
+  print("it's better without escape character")
+}
