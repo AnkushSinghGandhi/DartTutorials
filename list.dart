7081a5ee678b2df5a94b1de64e2c32b7c49163bf
muskan-pro <72640928+muskan-pro@users.noreply.github.com>
2020-10-12 04:01:51 +0530

    Delete list.dart

--- list.dart
+++
@@ -1,8 +0,0 @@
-main(){
-  var listofvegies = ['potato','carrot'];
-  listofvegies.addall(['cucumber','tomato']);
-  print(listofvegies);
-  listofvegies.removeAt(1);
-  print(listofvegies);
-  listofvegies.clear()
-}
